dd9ebc70bd91fd2571015f0ed516d57c83c03135
ohNegative <typeo80@outlook.com>
2019-12-26 19:00:57 -0600

    semicolons, semicolons

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { navigate } from 'gatsby-link'
-import Layout from '../templates/contact'
+import React from 'react';
+import { navigate } from 'gatsby-link';
+import Layout from '../templates/contact';
 
 function encode(data) {
   return Object.keys(data)
